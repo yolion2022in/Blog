e2a0eac452937906a2e90854772f1c2273d703c9
yolion08 <yolion2022@gmail.com>
2022-11-14 12:08:11 +0800

    webAPI route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function () {   //Closure Function
     return view('welcome');
 });
+
+Route::get('/hello','App\Http\Controllers\SiteController@hello');
