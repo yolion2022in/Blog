abc991a939ee5004c626bd28cd08b6780f66129c
yolion08 <yolion2022@gmail.com>
2022-11-22 04:30:06 +0800

    首改 Fake Pchome 失敗

---
+++ resources/views/ec/master/home.blade.php
@@ -0,0 +1,19 @@
+<html lang="zh-TW">
+  <head>
+    @yield('meta')
+
+    @yield('link')
+
+    <title>@yield('title', 'Fake PChome 線上購物') {{-- title給預設值 --}}</title>
+    
+    @yield('script')
+
+    @yield('meta2')
+  </head>
+  
+  <body>
+    @section('content')
+        <div style='color:goldenrod'>Master Fake Pchome</div>
+    @show
+  </body>
+</html>
